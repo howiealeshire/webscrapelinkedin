00e7e55614fb7b802950c2d86224f71aafb7725f
Howie <howie@Howies-MacBook-Pro.local>
2020-07-29 14:02:12 -0400

    first commit

---
+++ webscrapermilestone1.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import sys
+import re
+import urllib.request
+import pandas as pd
+import pprint
+import collections
+import requests
+import time
+import xlsxwriter
+import xlrd
+import csv
+from fake_useragent import UserAgent
+import os
+import random
+import math
+import pickle
+from datetime import datetime
+from itertools import groupby
+from collections import OrderedDict
+
+
+print("hello")
